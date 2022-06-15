b0322d70000a8c433696a24f38b53ed449437507
Bryan Pumisacho <bryan@gmail.com>
2022-06-14 21:46:26 -0500

    Middleweres

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
 //Ejecucion del servidor
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+
 const app = express();
 
 //settings
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
-//se ejecuta en el puerto 3000
+//middleweres
+app.use(morgan('dev'));
+app.use(myConnection(mysql,{
+    host: 'localhost',
+    user: 'root',
+    password: 'contraseña',
+    port: 3306,
+    database: 'crudnodejsmysql'
+},'single'));
+//routes
+
 app.listen(app.get('port'), () => {
-    console.log('');
+    console.log('Server on port 3000');
 });
 
 
